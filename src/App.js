b51e77d14295b7b751864c561bf6dd61df763d3e
Janik Matzel <janik.matzel@telekom.de>
2022-02-03 17:36:06 +0100

    Put in some content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Matthias Melk Automobile</h1>
+
+      <div className="Philosophie">
+        <h2>Philosophie und mein Versprechen</h2>
+        Als Kfz-Meister und Betriebswirt mit 35 Jahren Erfahrung in der Kfz-Branche habe ich mich darauf spezialisiert gezielt Fahrzeuge zu suchen und zu verkaufen die meinen hohen Ansprüchen entsprechen.
+        Ich vertreibe nur Fahrzeuge, die eine nachvollziehbare Historie haben und lückenlos nach den Herstellervorgaben gewartet worden sind und entsprechend dokumentiert (Serviceheft) wurden.
+        Diese Rahmenbedingungen gewährleisten dem Käufer eine sichere und sorgenfreie Mobilität.
+      </div>
+
+      <div>
+        <h2>Unser Anspruch</h2>
+        Wir sind ein inhabergeführter Kfz-Handelsbetrieb mit Menschen, die sich persönlich für sie engagieren und Verantwortung übernehmen.
+        Jeder Kontakt und Dialog mit Ihnen ist uns wichtig und bereitet uns Freude.
+        Als Interessent und Kunde stehen sie für uns im Mittelpunkt, verbunden mit dem Ziel das sie mit dem Fahrzeug und unseren Dienstleistungen rund um das Auto vollkommen zufrieden sind.
+      </div>
+
+      <div className="Impressum">
+        <h2>Impressum</h2>
+        <p>Angaben gemäß §5 TMG:</p>
+        <p></p>
+        <p>Matthias Melk Automobile</p>
+        <p>Kfz-Meister</p>
+        <p>Hauptstr. 20</p>
+        <p>82390 Eberfing / Deutschland</p>
+        <p></p>
+        <p>Vertreten durch:</p>
+        <p>Matthias Melk</p>
+        <p></p>
+        <p>Kontakt:</p>
+        <p>Tel.: +(49) 8802 9138808</p>
+        <p>Mobil: +(49)163 2904969</p>
+        <p>Fax: +(49) 8802 9139755</p>
+        <p>E-Mail: m.melk-automobile@t-online.de</p>
+        <p></p>
+        <p>Umsatzsteuer:</p>
+        <p>Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz:</p>
+        <p>DE 51627081394</p>
+      </div>
+
+
+      <div className="Kontakt">
+        <h2>Kontakt</h2>
+        <p></p>
+        <p>Matthias</p>
+        <p>Melk</p>
+        <p>Automobile</p>
+        <p></p>
+        <p>Postanschrift:</p>
+        <p>Hauptstr. 20</p>
+        <p>82390 Eberfing</p>
+        <p>Tel.: 08802/9138808</p>
+        <p>e-mail: m.melk-automobile@t-online.de</p>
+      </div>
     </div>
   );
 }
